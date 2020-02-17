b91b6c4515423e759d5f113eed144212610d0982
kennell <kevin@kennell.de>
2020-02-17 22:18:05 +0100

    bump to 1.2.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='flagz',
-    version='1.2.1',
+    version='1.2.2',
     py_modules=['flagz'],
     author='Kevin Kennell',
     author_email='kevin@kennell.de',
